d830230851d1d256f5d8ed0870618ba9de370398
NazarDonets <nazardonets@gmail.com>
2021-09-29 23:54:09 +0300

    added sorting buttons (for fun)

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,17 @@
 import data from './../menu.json';
 import articleTemp from './../templates/menu-card.hbs';
 
+var arraySort = require('array-sort');
+
 const refs = {
 	themeSwitch: document.querySelector('#theme-switch-toggle'),
 	body: document.querySelector('body'),
 	menuContainer: document.querySelector('.js-menu'),
+	buttonAsc: document.getElementById('sortAsc'),
+	buttonDesc: document.getElementById('sortDesc'),
 };
 
-const { themeSwitch, body, menuContainer } = refs;
+const { themeSwitch, body, menuContainer, buttonAsc, buttonDesc } = refs;
 
 const Theme = {
 	LIGHT: 'light-theme',
@@ -40,3 +44,29 @@ function createMarkup(data) {
 const articlesMarkup = createMarkup(data);
 
 menuContainer.insertAdjacentHTML('afterbegin', articlesMarkup);
+
+buttonAsc.addEventListener('click', () => {
+	menuContainer.innerHTML = '';
+	function createMarkup(data) {
+		return arraySort(data, 'price').map(articleTemp).join('');
+	}
+
+	const articlesMarkup = createMarkup(data);
+
+	menuContainer.insertAdjacentHTML('afterbegin', articlesMarkup);
+});
+
+buttonDesc.addEventListener('click', () => {
+	menuContainer.innerHTML = '';
+	function createMarkup(data) {
+		return arraySort(data, 'price', { reverse: true })
+			.map(articleTemp)
+			.join('');
+	}
+
+	const articlesMarkup = createMarkup(data);
+
+	menuContainer.insertAdjacentHTML('afterbegin', articlesMarkup);
+});
+
+// arraySort(array, comparisonArgs);
